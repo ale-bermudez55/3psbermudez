eee648b999c0c66fded6f599a3eefacdaccdb23d
ale-bermudez55 <36197197+ale-bermudez55@users.noreply.github.com>
2018-05-22 09:13:02 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+<meta charset='UTF-8'>
+<link rel="stylesheet" type"text/css" href="css/registro.css">
+
+<title> Registro de usuarios </title>
+<h2> Registro de usuarios </h2>
+<p>Completa el formulario con los datos solicitados: </p>
+<form action="alta_registro.php" method="post">
+<br>
+        <div>
+                <label for="name">Nombre:</label>
+                <input type="text" id="name"/>
+        <Br></Br>
+        </div>
+
+        <div>
+        <label for="apaterno">Apellido Paterno:</label>
+        <input type="text" id="apaterno"/>
+        <br></br> 
+	</div> 
+
+	<div>
+		<label for="amaterno">Apellido Materno:</label>
+		<input type="text" id="amaterno"/>
+		<br></br>
+	</div>
+
+    <div>
+   	<label for="tel">Telefono:</label>
+    	<input type="text" id="tel" />
+        <br></br>
+    </div>
+
+    <div>
+        <label for="mail">E-mail:</label>
+        <input type="email" id="mail" />
+        <br></br>
+    </div>
+
+        <div>
+                <input type="submit" value="Enviar">
+<br></br>
+</div>
+
+</form>
+</head>
+</html>
