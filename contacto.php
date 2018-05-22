c50061be443aae3b76c3a5e5da68ecb5cdf9fd97
ale-bermudez55 <36197197+ale-bermudez55@users.noreply.github.com>
2018-05-22 13:45:38 -0500

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contacto</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<!--Cabecera-->
+<div class="barra">
+<center><h1>Contactanos</h1></center>
+</div>
+<div class="contenido">
+<h2>Contacto</h2>
+<h3>Queremos saber tu opinión</h3> 
+<a href="alejonas55@gmail.com">Puedes enviarnos tus comentarios aqui</a>
+</div>
+
+<div class="pie">
+<a href="index.php">Inicio</a>
+<a href="contacto.php">Contacto</a>
+<a href="consulta.php">Consulta de usuarios</a>
+<a href="creditos.php">Creditos</a>
+</div>
+</body>
+</html>
